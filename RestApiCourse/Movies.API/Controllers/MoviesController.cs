f1a75f0feb60b58f2a62f626c5d54200d1ed81b4
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-06-25 19:21:17 +0000

    Adding initial chamges for controller

---
+++ RestApiCourse/Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.API.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        return Ok(request);
+    }
+
+}
--- RestApiCourse/Movies.Application/Models/Movie.cs
+++ RestApiCourse/Movies.Application/Models/Movie.cs
@@ -4,5 +4,8 @@ namespace Movies.Application.Models;
 
 public class Movie
 {
-
+    public required Guid Id { get; init; }
+    public required string Title { get; set; }
+    public required int YearOfRelease { get; set; }
+    public required List<string> Generes { get; init; } = new();
 }
