1a10b8425940a73e519eef0cc6fd347f06c392ab
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-07-09 15:56:32 +0000

    Refactor Genre entity to use shadow property for CreatedDate and update GenreMapping configuration

--- EntityFrameworkCore/EntityFrameworkCoreDoodle/DatabaseScript.sql
+++ EntityFrameworkCore/EntityFrameworkCoreDoodle/DatabaseScript.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE MoviesDB
+--CREATE DATABASE MoviesDB
 
 --DROP TABLE dbo.Movie_Actors;
 --DROP TABLE dbo.Movie_Director;
 --DROP TABLE Genres;
 --DROP TABLE Movies;
-
+Use MoviesDB;
 Select * from Genres;
--- EntityFrameworkCore/EntityFrameworkCoreDoodle/EFcore.API/Data/EntityMapping/GenreMapping.cs
+++ EntityFrameworkCore/EntityFrameworkCoreDoodle/EFcore.API/Data/EntityMapping/GenreMapping.cs
@@ -11,13 +11,19 @@ public class GenreMapping : IEntityTypeConfiguration<Genre>
     {
         builder.HasData(new Genre { Id = 1, Name = "Action" });
 
-        builder.Property(g => g.CreatedDate)
+        //created date is a shadow property
+        //it is not part of the model class
+        //it is not mapped to any property in the model class
+        //it is only used to store the created date in the database
+        builder.Property<DateTime>("CreatedDate")
+            .HasColumnName("CreatedAt")
+            //generating value on the SQL side by specifying a function
+            .HasDefaultValueSql("getdate()");
             //Generating value on the client side
             //this will only run when the entity is added to the context with unassigned value
             //does not work for seeded data or migrated data
             //only works when adding an entity to the context
             //.HasValueGenerator<CreatedDateGenerator>();
-        //generating value on the SQL side by specifying a function
-        .HasDefaultValueSql("getdate()");
+
     }
 }
