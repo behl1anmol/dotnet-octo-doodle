ef3e80be24d4e7b80441f4ee2d829558bd1cf2f3
Anmol <behl1anmol@gmail.com>
2025-07-28 13:29:37 +0530

    Add Address, Customer, and Product entities with relationships; scaffold AdventureWorks context

---
+++ EntityFrameworkCore/EntityFrameworkCoreDoodle/EFCore.DatabaseFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EFCore.DatabaseFirst.Context;
+
+Console.WriteLine("Hello, World!");
+
+var context = new AdventureWorksContext();
+
+var query = context.Customers.Where(c=>c.FirstName.StartsWith("C"))
+    .Select(c => new
+    {
+        c.FirstName,
+        c.LastName,
+        c.SalesOrderHeaders.Count
+    });
+
+foreach (var item in query)
+{
+    Console.WriteLine($"Customer: {item.FirstName} {item.LastName}, Orders: {item.Count}");
+}    
